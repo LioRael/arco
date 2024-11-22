3392707fd896daa02a7f89d3107e4a46410fab5b
LioRael <621862169@qq.com>
2024-11-22 16:25:43 +0800

    fix(router): remove unnecessary prefix from sponsors route

--- packages/api/src/router/index.ts
+++ packages/api/src/router/index.ts
@@ -2,7 +2,7 @@ import { pub } from '../orpc'
 import { listSponsors } from './sponsors'
 
 export const router = pub.router({
-  sponsors: pub.tags('Sponsors').prefix('/sponsors').router({
+  sponsors: pub.tags('Sponsors').router({
     list: listSponsors,
   }),
 })
